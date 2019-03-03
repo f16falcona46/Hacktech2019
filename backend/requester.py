2a1eb9a1331b8f960c753d3aa673aa1265482d15
HSQ8 <hongsenqin@gmail.com>
2019-03-03 05:31:26 -0800

    works now

--- backend/requester.py
+++ backend/requester.py
@@ -1,6 +1,7 @@
 import requests 
 import time
 import random
+import numpy as np
 
 URL = "http://maps.googleapis.com/maps/api/geocode/json"
   
@@ -19,10 +20,27 @@ def get_lat_long_itude(low,high):
 def get_magnitude(low,high):
     return random.uniform(low,high)
 
-def generate_timestamps(n, tmin, tmax): 
+def generate_timestamps(): 
     timestamp = time.time() * np.power(10., 3.) 
     return timestamp
 
-url = make_request_url(0,0,0,0,0)
-r = requests.get(url)
-print(r.text)
+def get_altitude():
+    return 0
+
+
+def send_request(url):
+    
+    r = requests.get(url)
+    print(r.text)
+
+num_requests = 10
+urls = [make_request_url(
+    get_magnitude(85,90),
+    get_lat_long_itude(33,35),
+    get_lat_long_itude(-119,-117),
+    generate_timestamps(),
+    get_altitude()
+    ) for i in range(num_requests)]
+
+for i in urls:
+    send_request(i)
