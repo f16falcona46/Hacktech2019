e563f92cc11e444da8b6bda044ac231605150860
HSQ8 <hongsenqin@gmail.com>
2019-03-03 08:36:32 -0800

    update

--- backend/requester.py
+++ backend/requester.py
@@ -1,7 +1,9 @@
-import requests 
+import requests
 import time
 import random
 import numpy as np
+import math
+from haversine import haversine
 
 URL = "http://maps.googleapis.com/maps/api/geocode/json"
   
@@ -20,27 +22,55 @@ def get_lat_long_itude(low,high):
 def get_magnitude(low,high):
     return random.uniform(low,high)
 
-def generate_timestamps(): 
-    timestamp = time.time() * np.power(10., 3.) 
-    return timestamp
+def generate_timestamps():
+    timestamp = time.time() * 10**3 + 10**4 + random.randint(0,20)# 10 second offset
+    return int(timestamp)
 
 def get_altitude():
     return 0
 
-
-def send_request(url):
-    
+def send_request(url):    
     r = requests.get(url)
     print(r.text)
+    return r.text
 
-num_requests = 10
+
+num_requests = 5
+
+s_speed = 343.2
+lowlat = 33
+lowlong = -119
+latrange = 0.005
+origin = (get_lat_long_itude(lowlat, lowlat + latrange), get_lat_long_itude(lowlong, lowlong + latrange))
+origin_time = generate_timestamps()
+
+
+gunshots = [(get_lat_long_itude(lowlat, lowlat + latrange), get_lat_long_itude(lowlong, lowlong + latrange)) for i in range(num_requests)]
+
 urls = [make_request_url(
     get_magnitude(85,90),
-    get_lat_long_itude(33,35),
-    get_lat_long_itude(-119,-117),
-    generate_timestamps(),
+    i[0],
+    i[1],
+    origin_time + int(haversine(i, origin,"m") * 10**3 / s_speed),
     get_altitude()
-    ) for i in range(num_requests)]
+    ) for i in gunshots]
 
 for i in urls:
     send_request(i)
+
+
+time.sleep(3) 
+urlreturn ="https://jasonli.us/cgi-bin/hacktech2019_return.cgi?"
+predictionlist = send_request(urlreturn).split(',')
+# prediction = (float(predictionlist[0]), float(predictionlist[1]))
+
+print("prediction")
+print(predictionlist)
+print("actual")
+print(origin)
+print("actual time")
+print(origin_time)
+
+print(gunshots)
+
+
