6d64bd21ef866447d75b97d8e0931bb271f33f7a
HSQ8 <hongsenqin@gmail.com>
2019-03-03 05:22:32 -0800

    Update requester.py

--- backend/requester.py
+++ backend/requester.py
@@ -1,5 +1,6 @@
 import requests 
-  
+import time
+import random
 
 URL = "http://maps.googleapis.com/maps/api/geocode/json"
   
@@ -17,6 +18,10 @@ def get_lat_long_itude(low,high):
     return random.uniform(low,high)
 def get_magnitude(low,high):
     return random.uniform(low,high)
+
+def generate_timestamps(n, tmin, tmax): 
+    timestamp = time.time() * np.power(10., 3.) 
+    return timestamp
 
 url = make_request_url(0,0,0,0,0)
 r = requests.get(url)
