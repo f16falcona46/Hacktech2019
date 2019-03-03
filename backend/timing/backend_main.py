52031fbc7cd0c89845271f064f9363ea7b5280de
Jason Li <li07.jason@gmail.com>
2019-03-03 08:36:04 -0800

    threading fix

--- backend/timing/backend_main.py
+++ backend/timing/backend_main.py
@@ -6,20 +6,22 @@ import sound_sensor_db as sdb
 import tdoa_locate as tdoa
 from threading import Thread, Event
 import numpy as np
+from datetime import datetime, timezone
 
 min_acc = 20
 
 def test_tdoa_goodness(conn):
     s, t_first = sdb.dump_for_delay(conn)
     n = s.shape[0]
-    print(repr(s))
     if n >= 4:
         loc = s[:, :3]
         dist = s[:, 3]
+        print(loc, dist)
         x_s, acc = tdoa.shuffle_tdoa_locate(loc, dist, n // 2)
         print(acc)
         if acc < min_acc:
-            sdb.add_event_to_db(x_s[0], x_s[1], x_s[2], t_first)
+            print(x_s[0], x_s[1], x_s[2], t_first)
+            sdb.add_event_to_db(x_s[0], x_s[1], x_s[2], t_first, conn=conn)
 
 def test_hotspot_goodness():
     return
@@ -32,7 +34,7 @@ class DbCleanerThread(Thread):
     def run(self):
         self.conn = sdb.newconn()
         while not self.stopped.wait(2.0):
-            sdb.clean_db(conn=self.conn)
+            sdb.clean_db(datetime.now(timezone.utc), conn=self.conn)
             test_tdoa_goodness(conn=self.conn)
 
 stop = Event()
