00dc82c0a89ee3afd942a54716534729d1516f92
Jason Li <li07.jason@gmail.com>
2019-03-03 03:12:52 -0800

    change coordinate system

--- backend/timing/backend_main.py
+++ backend/timing/backend_main.py
@@ -7,7 +7,8 @@ import sound_sensor_db as sdb
 def app(env, start_response):
     start_response("200 OK", [("Content-type", "text/html; charset=utf-8")])
     q = parse_qs(env["QUERY_STRING"])
-    sdb.add_reading_to_db(float(q["t"]), float(q["m"]), float(q["lat"]), float(q["long"]), float(q["alt"])) #FIX DATE CONVERSION!
+    print(q)
+    sdb.add_reading_to_db(int(q["t"][0]), float(q["m"][0]), float(q["lat"][0]), float(q["long"][0]), float(q["alt"][0])) #FIX DATE CONVERSION!
     return []
 
 httpd = wsgiref.simple_server.make_server("localhost", 8080, app)
